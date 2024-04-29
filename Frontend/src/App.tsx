c6c5ac7595e5d52831c76579ac4c5941cd42c5f4
Ewy Rostam <ewy.rost@gmail.com>
2024-04-29 10:21:09 +0200

    Add user type

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,6 +5,12 @@ import { deleteCookie } from './helpers/CookieHelpers';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 
+export type User = {
+  picture: string;
+  name: string;
+  email: string;
+};
+
 function App() {
   const [profile, setProfile] = useState<User>();
 
