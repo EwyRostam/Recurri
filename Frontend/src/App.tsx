4c202200f550179f4533486f2afc9b34a7ee02a9
Axel Petersson <axel.818@gmail.com>
2024-04-29 10:48:33 +0200

    cleans up app.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,24 +1,9 @@
-import { useState } from 'react';
 import './App.css'
 import Home from './components/Home/Home'
-import { deleteCookie } from './helpers/CookieHelpers';
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
 
-export type User = {
-  picture: string;
-  name: string;
-  email: string;
-};
+
 
 function App() {
-  const [profile, setProfile] = useState<User>();
-
-  const logOut = () => {
-    setProfile(undefined);
-    deleteCookie("access_token");
-    window.location.reload();
-  };
   return (
     <div>
      <Home/>
