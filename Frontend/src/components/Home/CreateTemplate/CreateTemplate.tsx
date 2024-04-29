f544d6ec8f575b9ae0c43086da69aea435199810
Ewy Rostam <ewy.rost@gmail.com>
2024-04-29 15:08:58 +0200

    Try to fix usemutation

--- Frontend/src/components/Home/CreateTemplate/CreateTemplate.tsx
+++ Frontend/src/components/Home/CreateTemplate/CreateTemplate.tsx
@@ -4,11 +4,12 @@ import { CalendarEvent } from "./WeekTable/Event/CalendarEvent";
 import { convertToGoogle } from "./helpers";
 import { useNavigate } from "react-router-dom";
 import { saveCalendarTemplate } from "../../../api/TemplateApi";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export type Template = {
     userEmail: string;
     name: string;
-    weeks : Week[];
+    weeks: Week[];
 }
 
 export type Week = {
@@ -20,31 +21,34 @@ type Props = {
     setTemplates: Dispatch<SetStateAction<Template[]>>
 }
 
-function CreateTemplate({setTemplates} : Props) {
+function CreateTemplate({ setTemplates }: Props) {
     const navigate = useNavigate();
     const [weeks, setWeeks] = useState<Week[]>([{
         number: 1,
-        events : [{
+        events: [{
             name: "",
             description: "",
             day: 1,
-            startTime : "",
-            endTime : "",
-            recurrence : ""
+            startTime: "",
+            endTime: "",
+            recurrence: ""
         }]
     }]);
 
     const CustomRef = useRef<HTMLDialogElement>(null)
 
     const handleAddWeek = () => {
-        const newWeek = { number: 1, events: [{
-        name: "",
-        description: "",
-        day: 0,
-        startTime: "",
-        endTime: "",
-        recurrence: ""}
-    ] };
+        const newWeek = {
+            number: 1, events: [{
+                name: "",
+                description: "",
+                day: 0,
+                startTime: "",
+                endTime: "",
+                recurrence: ""
+            }
+            ]
+        };
         setWeeks((prevWeeks) => [...prevWeeks, newWeek]);
     };
 
@@ -62,34 +66,47 @@ function CreateTemplate({setTemplates} : Props) {
         setWeeks(() => updatedWeeks);
     };
 
+    const queryClient = useQueryClient();
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const { templateName } = e.target as typeof e.target & {
             templateName: { value: string };
         };
-    
+
         const template: Template = {
             userEmail: sessionStorage.getItem("email")!,
             name: templateName.value,
-            weeks : weeks
+            weeks: weeks
         };
 
-        saveCalendarTemplate(template);
+        onSaveTemplate.mutate(template);
+
+        useMutation(saveCalendarTemplate,
+            {
+                onSuccess: () => {
+                    queryClient.invalidateQueries({ queryKey: ['templates'] })
+                },
+            });
         navigate("/home")
     }
 
-    
+    const onSaveTemplate = (template: Template) => {
+        saveCalendarTemplate(template)
+    }
 
-    
+
+
+
     return (
         <section className="px-4">
 
             <form action="" onSubmit={handleSubmit} className="flex flex-col gap-4">
-                <input type="text" name="templateName" className="input input-bordered w-full input-sm max-w-xs" placeholder="Template name"/>
+                <input type="text" name="templateName" className="input input-bordered w-full input-sm max-w-xs" placeholder="Template name" />
                 <button type="button" onClick={handleAddWeek} className="btn btn-sm max-w-48">+ Add Week</button>
                 <WeekTable weeks={weeks} handleAddEvent={handleAddEvent} setWeeks={setWeeks} CustomRef={CustomRef} />
                 <input type="submit" className="btn btn-sm mt-4 max-w-48" value="Create Template" />
-                <button className="btn btn-sm max-w-48" onClick={()=>convertToGoogle(weeks, new Date())}> Convert To google </button>
+                <button className="btn btn-sm max-w-48" onClick={() => convertToGoogle(weeks, new Date())}> Convert To google </button>
             </form>
 
             <dialog id="my_modal_3" className="modal z-20" ref={CustomRef}>
