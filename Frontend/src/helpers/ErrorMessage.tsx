a6b7ecbe1593b7e22357c23fadfeb50e21bfa6e4
alexDanilMain <alexdanilwork@gmail.com>
2024-05-01 14:06:38 +0200

    adds colors and fixes routing

---
+++ Frontend/src/helpers/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const ErrorMessage = () => {
+    return (
+        <div className="w-full h-full flex items-center justify-center flex-col">
+            <p>An error has ocurry in recurry. Try refreshing. Contact us if the issues recurries</p>
+        </div>
+    )
+}
+
+export default ErrorMessage
---
+++ Frontend/src/helpers/LoadingMessage.tsx
@@ -0,0 +1,12 @@
+
+
+const LoadingMessage = () => {
+    return (
+        <div className="w-full h-full flex items-center justify-center flex-col">
+            <p>Loading...</p>
+            <span className="loading loading-spinner loading-lg"></span>
+        </div>
+    )
+}
+
+export default LoadingMessage
